5dd700e0bc865b232b8fcfa232159ca106bcc152
danilo.gonzalez.ardon <danilo.gonzalez.ardon@outlook.com>
2025-08-28 17:01:36 -0600

    menu

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
+import Menu from "@/components/Menu";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
     children,
 }: {
@@ -6,9 +10,20 @@ export default function DashboardLayout({
     return (
         <section className="h-screen flex">
             {/* LEFT */}
-            <div className="w-1/6">L</div>
+            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+                <Link
+                    href="/"
+                    className="flex items-center justify-center lg:justify-start gap-2"
+                >
+                    <Image src="/logo.png" alt="logo" width={32} height={32} />
+                    <span className="hidden lg:block">SchoolLama</span>
+                </Link>
+                <Menu />
+            </div>
             {/* RIGHT */}
-            <div className="w-5/6">R</div>
+            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+                R{children}
+            </div>
         </section>
     );
 }
