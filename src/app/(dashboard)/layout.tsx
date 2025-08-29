b64bacdae4b4d32a3bd9a476b800d3fefbf243c1
danilo.gonzalez.ardon <danilo.gonzalez.ardon@outlook.com>
2025-08-29 15:19:08 -0600

    navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Menu from "@/components/Menu";
+import NavBar from "@/components/NavBar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -22,7 +23,8 @@ export default function DashboardLayout({
             </div>
             {/* RIGHT */}
             <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
-                R{children}
+                <NavBar />
+                {children}
             </div>
         </section>
     );
