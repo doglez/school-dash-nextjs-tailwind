15b658a52996cd7627d795a94d32d5aa3d2b6c55
danilo.gonzalez.ardon <danilo.gonzalez.ardon@outlook.com>
2025-10-22 14:49:50 -0600

    FinanceChart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
@@ -26,7 +27,9 @@ const AdminPage = () => {
                     </div>
                 </div>
                 {/* BOTTON CHART  */}
-                <div></div>
+                <div className="w-full h-[500px]">
+                    <FinanceChart />
+                </div>
             </div>
             {/* RIGHT */}
             <div className="w-full lg:w-1/3">RIGHT</div>
